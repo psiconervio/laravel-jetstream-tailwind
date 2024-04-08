1238e2c0b0e553eed4e4050680e82d51e690a9e1
psiconervio <psiconervio@gmail.com>
2024-04-08 16:37:37 -0300

    asd

--- proyectotailwind/resources/views/livewire/nueva-pagina.blade.php
+++ proyectotailwind/resources/views/livewire/nueva-pagina.blade.php
@@ -1,5 +1,5 @@
 <div>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}asdadsa
+    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}asdadsaasdasd
         <livewire:hello-world/>
 
 </div>
